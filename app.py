d5239d1addb4bedaef32d22ac26641222847e0b0
Ahmed Maher <118073597+AhmedMaherTohmay@users.noreply.github.com>
2025-07-08 21:09:00 +0300

    cors

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # Add CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # You can specify trusted domains instead of "*"
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
